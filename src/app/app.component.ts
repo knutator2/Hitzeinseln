19d6638572aaf0367d81cad9a23afb04e93228f7
Knut Perseke <knut.perseke@gmail.com>
2024-05-26 10:30:24 +0200

    adds gitignore

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {LeafletModule} from "@asymmetrik/ngx-leaflet";
+import * as lng from 'leaflet'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LeafletModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'Hitzeinseln';
+  options = {
+    layers: [
+      lng.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: '...' })
+    ],
+    zoom: 8,
+    center: lng.latLng(51.233334, 6.783333)
+  };
 }
